15e12e9d56393349755ec4c1c493addb6ac3b358
CLASHERBROs <parigames@gmail.com>
2020-12-31 12:51:50 +0530

    Added react-animations

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react"
+import {fadeIn,bounce} from 'react-animations'
+import styled,{keyframes} from 'styled-components'
+import {Button} from '@material-ui/core'
+import './Home.css'
+const FadeIn = styled.div`animation: 5s ${keyframes `${fadeIn}`} infinite`;
+const Bounce = styled.div`animation: 2s ${keyframes `${bounce}`}  infinite`;
+const Home = ()=>{
+    const [check,setCheck] = useState(false);
+    return(
+        <div style={{backgroundColor:"black",color:"white"}}>
+       {check?(<div> <FadeIn>
+        <h1>This is React Babes</h1>
+        </FadeIn>
+    <Bounce>
+        <h1>Yay</h1>
+    </Bounce></div>):(<div><h1>Animations will come here</h1></div>)
+    }
+       
+
+    <Button color="primary" onClick={()=>{setCheck(!check)}}>Animate</Button>
+    </div>
+    )
+}
+
+export default Home;
